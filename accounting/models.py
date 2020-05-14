f0208defd70dc644c74db603b21c32374b7a41fb
mmafshari77 <mahdiafshari@aut.ac.ir>
2020-05-14 10:59:35 +0430

    First Models

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+# Assets = Liabilities + Owner's Equity
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    date = models.DateField()
+    assets = models.FloatField()
+    liablities = models.FloatField()
+    owner_equity = models.FloatField()
+    image = models.ImageField()
+
+
